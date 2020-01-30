9b22f76ab02f24815ab504d28ac8b21a4869ce4c
yuka_yamashita <yuka.yamashita@leverages.jp>
2020-01-30 15:41:37 +0900

    [mod]レビュー指摘、Controllerになんでも書かない。

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html lang="ja">
-<style type="text/css">
-    table, td, th { border: 1px #2b2b2b solid; width:1100px}
-</style>
 <head>
-    <title>Users/Index</title>
+    <style type="text/css">
+        table, td, th { border: 1px #2b2b2b solid; width:1100px}
+    </style>
+    <title>ユーザー情報</title>
 </head>
 <body>
 <h1>ユーザー情報</h1>
@@ -13,36 +13,32 @@
 <table>
     <tr>
         <th>ユーザーID</th>
-        <th>ログインID</th>
         <th>性</th>
         <th>名</th>
         <th>セイ</th>
         <th>メイ</th>
         <th>性別</th>
         <th>メールアドレス</th>
-        <th>ログイン状態</th>
-        <th>更新</th>
-        <th>削除</th>
+        <th></th>
+        <th></th>
     </tr>
-    //foreachつける
+    @foreach($users as $user)
         <tr>
-            <td></td>
-            <td></td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
+            <td>{{$user->id}}</td>
+            <td>{{$user->last_name}}</td>
+            <td>{{$user->first_name}}</td>
+            <td>{{$user->last_name_kana}}</td>
+            <td>{{$user->first_name_kana}}</td>
+            <td>{{$user->gender}}</td>
+            <td>{{$user->email}}</td>
             <td><a href="users/user->id/edit">更新</a></td>
             <td><a href="users/user->id/delete">削除</a></td>
         </tr>
+    @endforeach
 </table>
 </br>
 
 <h2>ユーザー情報操作履歴</h2>
-</body>
 <table>
     <tr>
         <th>操作対象ユーザー</th>
@@ -57,5 +53,5 @@
         <td>a</td>
     </tr>
 </table>
+</body>
 </html>
-
