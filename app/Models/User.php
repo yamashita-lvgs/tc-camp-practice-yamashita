22a8dd73908bc1c68fd6ee98c04ad4c02248d908
yuka_yamashita <yuka.yamashita@leverages.jp>
2020-02-03 17:56:39 +0900

    [mod]インデント

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,32 +1,31 @@
 <?php
+namespace App\Models;
 
-    namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-    use Illuminate\Database\Eloquent\Model;
-
-    /**
-     * ユーザーテーブルのモデルクラス
-     * @package App\Models
-     */
-    class User extends Model
+/**
+ * ユーザーテーブルのモデルクラス
+ * @package App\Models
+ */
+class User extends Model
+{
+    public function getFullNameAttribute()
     {
-        public function getFullNameAttribute()
-        {
-            return "{$this->last_name} {$this->first_name}";
-        }
-
-        public function getOperatedFullNameAttribute()
-        {
-            return "{$this->Operated_user->last_name} {$this->Operated_user->first_name}";
-        }
-
-        public function getOperatingFullNameAttribute()
-        {
-            return "{$this->Operating_user->last_name} {$this->Operating_user->first_name}";
-        }
-
-        public function getOperationAttribute()
-        {
-            return OPRATIONS_LIST[$this->operation_id];
-        }
+        return "{$this->last_name} {$this->first_name}";
     }
+
+    public function getOperatedFullNameAttribute()
+    {
+        return "{$this->Operated_user->last_name} {$this->Operated_user->first_name}";
+    }
+
+    public function getOperatingFullNameAttribute()
+    {
+        return "{$this->Operating_user->last_name} {$this->Operating_user->first_name}";
+    }
+
+    public function getOperationAttribute()
+    {
+        return OPRATIONS_LIST[$this->operation_id];
+    }
+}
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,56 +1,56 @@
 <!doctype html>
 <html lang="ja">
-<head>
-    <title>ユーザー一覧</title>
-    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
-</head>
-<body>
-    <h1>ユーザー一覧</h1>
-    <a href="users/create">新規ユーザー登録</a>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>名前</th>
-            <th>権限</th>
-            <th>メールアドレス</th>
-            <th>登録者</th>
-            <th>登録日時</th>
-            <th>更新者</th>
-            <th>更新日時</th>
-            <th></th>
-            <th></th>
-        </tr>
-        @foreach($users as $user)
+    <head>
+        <title>ユーザー一覧</title>
+        <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    </head>
+    <body>
+        <h1>ユーザー一覧</h1>
+        <a href="users/create">新規ユーザー登録</a>
+        <table>
             <tr>
-                <td>{{ $user->id }}</td>
-                <td>{{ $user->full_name }}</td>
-                <td>権限名いれる</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->created_user_id }}</td>
-                <td>{{ $user->created_at }}</td>
-                <td>{{ $user->updated_user_id }}</td>
-                <td>{{ $user->updated_at }}</td>
-                <td><a href="users/{{$user->id}}/edit">更新</a></td>
-                <td><a href="users/{{$user->id}}/delete">削除</a></td>
+                <th>ID</th>
+                <th>名前</th>
+                <th>権限</th>
+                <th>メールアドレス</th>
+                <th>登録者</th>
+                <th>登録日時</th>
+                <th>更新者</th>
+                <th>更新日時</th>
+                <th></th>
+                <th></th>
             </tr>
-        @endforeach
-    </table>
-    <h1>ユーザー情報操作履歴</h1>
-    <table>
-        <tr>
-            <th>操作日時</th>
-            <th>操作種別</th>
-            <th>操作対象ユーザー</th>
-            <th>操作実施ユーザー</th>
-        </tr>
-        @foreach($userOperationHistories as $userOperationHistory)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->full_name }}</td>
+                    <td>権限名いれる</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_user_id }}</td>
+                    <td>{{ $user->created_at }}</td>
+                    <td>{{ $user->updated_user_id }}</td>
+                    <td>{{ $user->updated_at }}</td>
+                    <td><a href="users/{{$user->id}}/edit">更新</a></td>
+                    <td><a href="users/{{$user->id}}/delete">削除</a></td>
+                </tr>
+            @endforeach
+        </table>
+        <h1>ユーザー情報操作履歴</h1>
+        <table>
             <tr>
-                <td>{{ $userOperationHistory->actioned_at }}</td>
-                <td>{{ $userOperationHistory->operation}}</td>
-                <td>{{ $userOperationHistory->operated_full_name }} </td>
-                <td>{{ $userOperationHistory->operating_full_name }}</td>
+                <th>操作日時</th>
+                <th>操作種別</th>
+                <th>操作対象ユーザー</th>
+                <th>操作実施ユーザー</th>
             </tr>
-        @endforeach
-    </table>
-</body>
+            @foreach($userOperationHistories as $userOperationHistory)
+                <tr>
+                    <td>{{ $userOperationHistory->actioned_at }}</td>
+                    <td>{{ $userOperationHistory->operation}}</td>
+                    <td>{{ $userOperationHistory->operated_full_name }} </td>
+                    <td>{{ $userOperationHistory->operating_full_name }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </body>
 </html>
