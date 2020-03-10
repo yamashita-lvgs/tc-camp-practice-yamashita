784c357c82a80ba3b866c7d8b4efb9c1b52b0a33
yuka yamashita <59597560+yamashita-lvgs@users.noreply.github.com>
2020-03-10 09:55:03 +0900

    Update app/Http/Middleware/UserLoginSession.php
    Co-Authored-By: YukiNei-Lev <44665501+YukiNei-Lev@users.noreply.github.com>

--- app/Http/Middleware/UserLoginSession.php
+++ app/Http/Middleware/UserLoginSession.php
@@ -15,7 +15,7 @@ class UserLoginSession
      */
     public function handle($request, Closure $next)
     {
-        if ( !session()->has('user_id') ) {
+        if (!session()->has('user_id')) {
             return redirect('login');
         }
 
